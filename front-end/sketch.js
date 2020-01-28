94814ddc65c93b8a1af7b04bfaa50ba4bf12f90d
Botond Hegyi <hbotond97@gmail.com>
2020-01-28 23:49:00 +0200

    optimize drawing

--- front-end/sketch.js
+++ front-end/sketch.js
@@ -2,21 +2,42 @@ let images = [];
 let images2 = [];
 let canDraw = false;
 
-function setup() {
-    createCanvas(windowWidth-100, windowHeight-10, WEBGL);
+let extraCanvas;
 
-    document.getElementById("loadBtn").onclick = () => {
-        for (let i = 1; i <= 20; i++) {
-                let img = loadImage('images/row-images/1k/' + i + '.jpg');
+let myWindowWidth;
+let myWindowHeight;
+
+function setup() {
+    myWindowWidth = windowWidth * 89 / 100;
+    myWindowHeight = windowHeight - 10;
+    createCanvas(myWindowWidth, myWindowHeight, WEBGL);
+
+    extraCanvas = createGraphics(myWindowWidth, myWindowHeight, WEBGL);
+    extraCanvas.background(255, 0, 0);
+    extraCanvas.translate(-myWindowWidth / 2, -myWindowHeight / 2);
+
+    document.getElementById('loadBtn').onclick = () => {
+        document.getElementById('loading').style.visibility='visible';
+
+
+        for (let i = 1; i <= 200; i++) {
+            let img = loadImage('images/row-images/10k/' + i + '.jpg');
             images.push(img);
         }
 
         setTimeout(() => {
             console.log(`start cutting, images: ${images.length}`);
+            let x = 0, y = 0;
             for (let i = 0; i < images.length; i++) {
-                    for (let j = 0; j < 50; j++) {
-                        images2.push(images[i].get(50 * j, 0, 50, 50));
-                    }
+                for (let j = 0; j < 50; j++) {
+                    let img = images[i].get(50 * j, 0, 50, 50);
+                    images2.push(img);
+
+                    extraCanvas.image(img, x, y);
+                    x += 50;
+                }
+                x = 0;
+                y += 50;
             }
 
             console.log(`cutting done, images2: ${images2.length}`);
@@ -24,31 +45,24 @@ function setup() {
         }, 1000);
 
         console.log(`load done, images: ${images.length}`);
+
+        document.getElementById('loading').style.visibility='hidden';
     };
 
-    document.getElementById("rndBtn").onclick = () => {
-      shuffle(images2, true);
+    document.getElementById('rndBtn').onclick = () => {
+        shuffle(images2, true);
     };
 
-    document.getElementById("revBtn").onclick = () => {
-      images2.reverse();
+    document.getElementById('revBtn').onclick = () => {
+        images2.reverse();
     };
 }
 
 function draw() {
     background(0);
     orbitControl();
+    translate(-myWindowWidth / 2, -myWindowHeight / 2);
     let x = -windowWidth / 2;
     let y = -windowHeight / 2;
-    if (canDraw) {
-        for (let i = 0; i < images2.length; i++) {
-            image(images2[i], x, y);
-            x += 50;
-            // console.log((i % 50));
-            if ((i + 1) % 50 === 0) {
-                y += 50;
-                x = -windowWidth / 2;
-            }
-        }
-    }
+    image(extraCanvas, 0, 0);
 }
