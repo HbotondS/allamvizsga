6ca6f4d2aa9ca8bcc920b784d5ad13b807ff3653
Botond Hegyi <hbotond97@gmail.com>
2020-02-15 22:49:28 +0200

    the client now can upload images to the server

--- front-end/testing/sketch.js
+++ front-end/testing/sketch.js
@@ -6,14 +6,28 @@ function setup() {
 
     document.getElementById("title").onchange = () => {
         name = document.getElementById("title").value;
-    }
+    };
 
     document.getElementById("file").onchange = () => {
         file = document.getElementById("file").files[0];
-    }
+    };
 
-    document.getElementById("saveImage").onchange = () => {
-        // todo: saving the image
+    document.getElementById("saveImage").onclick = () => {
+        var postData = new FormData();
+        postData.append('name', name);
+        postData.append('image', file, file.name);
+        let url = 'http://127.0.0.1:8000/images/';
+        let method = 'POST';
+        const shouldBeAsync = true;
+
+        let request = new XMLHttpRequest();
+
+        request.onload = function () {
+            var status = request.status; // HTTP response status, e.g., 200 for "200 OK"
+            var data = request.responseText; // Returned data, e.g., an HTML document.
+        };
+        request.open(method, url, shouldBeAsync);
+        request.send(postData);
     }
 }
 
--- server/server/settings.py
+++ server/server/settings.py
@@ -38,12 +38,18 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'api'
+]
+
+CORS_ORIGIN_WHITELIST = [
+    "http://127.0.0.1:63343"
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
