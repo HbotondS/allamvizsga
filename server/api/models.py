f710476d5e6186b657ab98055725770cc8b755a9
HbotondS <hbotond97@gmail.com>
2020-03-04 00:01:09 +0200

    delete files when deleting objects from DB

--- server/api/models.py
+++ server/api/models.py
@@ -8,3 +8,12 @@ def upload_path(instance, filename):
 class Image(models.Model):
     folder = models.CharField(max_length=32, blank=False)
     image = models.ImageField(blank=False, null=False, upload_to=upload_path)
+
+
+from django.db.models.signals import pre_delete
+from django.dispatch.dispatcher import receiver
+
+@receiver(pre_delete, sender=Image)
+def mymodel_delete(sender, instance, **kwargs):
+    # Pass false so FileField doesn't save the model.
+    instance.image.delete(False)
