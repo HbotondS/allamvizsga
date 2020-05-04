9a49a4d17840fb3d3508d79c146c47c3a37ac4b7
HbotondS <hbotond97@gmail.com>
2020-05-04 17:50:22 +0300

    implement big image generator and randomizer

--- server/api/views.py
+++ server/api/views.py
@@ -4,13 +4,9 @@ from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.core.files import File
 from .models import ImageData, BigImageData
 from .serializers import ImageSerializer, BigImageSerializer
-import logging
 import random, json
 from PIL import Image
 from io import BytesIO
-
-
-logger = logging.getLogger(__name__)
 
 
 def index(request):
@@ -19,22 +15,40 @@ def index(request):
 
 def big(request):
     imglist = list(ImageData.objects.all())
-    img1 = Image.open(imglist[0].image)
-    img2 = Image.open(imglist[1].image)
     new_img = Image.new('RGB', (50*50, len(imglist) * 50))
     y_offset = 0
-    new_img.paste(img1, (0, y_offset))
+    img = Image.open(imglist[0].image)
+    new_img.paste(img, (0, y_offset))
     for i in range(1, len(imglist)):
         img = Image.open(imglist[i].image)
         y_offset += img.size[1]
         new_img.paste(img, (0, y_offset))
 
-    blob = BytesIO()
-    new_img.save(blob, img1.format)
-    BIG = BigImageData()
-    BIG.image.save('big.jpg', File(blob), save=False)
-    BIG.save()
-    return HttpResponse("Hello, world. You're at the big image index.")    
+    res = HttpResponse(content_type="image/jpeg")
+    new_img.save(res, "JPEG")
+    return res
+
+
+def randomImages(request):
+    imglist = list(ImageData.objects.all())
+    random.shuffle(imglist)
+    new_img = Image.new('RGB', (50*50, len(imglist) * 50))
+    y_offset = 0
+    img = Image.open(imglist[0].image)
+    new_img.paste(img, (0, y_offset))
+    for i in range(1, len(imglist)):
+        img = Image.open(imglist[i].image)
+        y_offset += img.size[1]
+        new_img.paste(img, (0, y_offset))
+
+    res = HttpResponse(content_type="image/jpeg")
+    new_img.save(res, "JPEG")
+    return res
+
+
+def histogram(request):
+    # todo: implement histogram generation after refactoring of the image store
+    return None
 
 
 class BigImageViewSet(viewsets.ModelViewSet):
