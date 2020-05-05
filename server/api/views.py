eb764a95675ab8c14817558fdd9c3f18534b2d66
HbotondS <hbotond97@gmail.com>
2020-05-05 17:38:02 +0300

    implement histogram generator

--- server/api/views.py
+++ server/api/views.py
@@ -53,10 +53,40 @@ def randomImages(request):
     return gen_img(imglist)
 
 
+# return the length of the longest element from the dictionary
+def GetMaxFlow(dict):        
+    pos=max(dict, key=lambda k: len(dict[k]))
+    return len(dict[pos])
+
+
 def histogram(request):
-    # todo: implement histogram generation after refactoring of the image store
-    print('test logging')
-    return HttpResponse("Hello, world. You're at the histogram index.")
+    # print('test logging')
+    # group images by date
+    img_dict = {}
+    imglist = list(ImageData.objects.all())
+    for img_data in imglist:
+        if img_data.date in img_dict:
+            img_dict[img_data.date].append(img_data.image)
+        else:
+            img_dict[img_data.date] = [img_data.image]
+    
+    height = GetMaxFlow(img_dict)
+    big_img = Image.new('RGB', (50*len(img_dict), 50*height))
+    x_offset = 0
+    for i in img_dict:
+        y_offset = 0
+        column_img = Image.new('RGB', (50, 50*height))
+        for j in img_dict[i]:
+            img = Image.open(j)
+            column_img.paste(img, (0, y_offset))
+            y_offset += img.size[1]
+        
+        big_img.paste(column_img, (x_offset, 50*(height - len(img_dict[i]))))
+        x_offset += column_img.size[0]
+
+    res = HttpResponse(content_type="image/jpeg")
+    big_img.save(res, "JPEG")
+    return res
 
 
 class BigImageViewSet(viewsets.ModelViewSet):
