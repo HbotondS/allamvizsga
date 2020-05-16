dbad11a66439388a11b1a1c8db037a6ac776f1dd
HbotondS <hbotond97@gmail.com>
2020-05-16 15:20:39 +0300

    test image resize with OpenCV

--- testing/testing.py
+++ testing/testing.py
@@ -2,22 +2,46 @@ import numpy as np
 import cv2
 import timeit
 from PIL import Image
+import os
 
 
-start = timeit.default_timer()
-row = []
-for j in range(200):
-    images = []
-    for i in range(1, 200):
-        images.append(cv2.imread("./images/single-images/40k/" + str(i) + ".jpg"))
-    row.append(cv2.hconcat(images))
+def big():
+    start = timeit.default_timer()
+    row = []
+    for j in range(200):
+        images = []
+        for i in range(1, 200):
+            images.append(cv2.imread("./images/single-images/40k/" + str(i) + ".jpg"))
+        row.append(cv2.hconcat(images))
 
-output = cv2.vconcat(row)
-cv2.imwrite('BIG.jpg', output)
-cv2.imshow('faszom', output)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    output = cv2.vconcat(row)
+    cv2.imwrite('BIG.jpg', output)
+    cv2.imshow('test', output)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    stop = timeit.default_timer()
+    print('Time: ', stop - start)
 
 
-stop = timeit.default_timer()
-print('Time: ', stop - start)
+def resize(folder, filename):
+    img = cv2.imread(folder + '/1k/' + filename, cv2.IMREAD_UNCHANGED)
+
+    # print('Original Dimensions : ', img.shape)
+    dim = (25, 25)
+    # resize image
+    resized = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
+    
+    # print('Resized Dimensions : ', resized.shape)
+    cv2.imwrite(folder + '/small/' + filename, resized)
+
+
+def list_files():
+    folder = 'images/testing'
+    for filename in os.listdir(folder + '/1k'):
+        # print(filename)
+        resize(folder, filename)
+
+
+# resize()
+list_files()
