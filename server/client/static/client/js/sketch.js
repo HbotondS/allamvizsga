9495f053fd41735a2ce1cfa215f421e636ee9f2d
HbotondS <hbotond97@gmail.com>
2020-05-23 20:12:43 +0300

    refactor button functions

--- server/client/static/client/js/sketch.js
+++ server/client/static/client/js/sketch.js
@@ -19,51 +19,67 @@ let img_loaded = false;
 
 const BACK_END_URL = 'http://127.0.0.1:8000';
 
+// load the images from the back-end
+function loadImages() {
+    const t0 = performance.now();
+    httpGet(BACK_END_URL + '/big', data => {
+        loadImage(BACK_END_URL + data, img => {
+            this.img = img;
+            this.img_loaded = true;
+
+            const t1 = performance.now();
+            print(`Loading images images took: ${Number((t1 - t0) / 1000).toFixed(2)} seconds.`);
+        })
+    })
+}
+
+// randomize the images
+function randomImages() {
+    const t0 = performance.now();
+    httpGet(BACK_END_URL + '/random', data => {
+        loadImage(BACK_END_URL + data, img => {
+            this.img = img;
+            this.img_loaded = true;
+
+            const t1 = performance.now();
+            print(`Random order images took: ${Number((t1 - t0) / 1000).toFixed(2)} seconds.`);
+        })
+    })
+}
+
+// sort the images in reverse order on the server
+function reverseImages() {
+    const t0 = performance.now();
+    httpGet(BACK_END_URL + '/reverse', data => {
+        loadImage(BACK_END_URL + data, img => {
+            this.img = img;
+            this.img_loaded = true;
+
+            const t1 = performance.now();
+            print(`Reverse order images took: ${Number((t1 - t0) / 1000).toFixed(2)} seconds.`);
+        })
+    })
+}
+
+// diplay the images on a histogram, grouped by dates
+function histogram() {
+    const t0 = performance.now();
+    httpGet(BACK_END_URL + '/histogram', data => {
+        loadImage(BACK_END_URL + data, img => {
+            this.img = img;
+            this.img_loaded = true;
+
+            const t1 = performance.now();
+            print(`Histogram images took: ${Number((t1 - t0) / 1000).toFixed(2)} seconds.`);
+        })
+    })
+}
+
 function setup() {
     zoom = canvasSize;
     myWindowWidth = windowWidth * 80 / 100;
     myWindowHeight = windowHeight - 50;
     createCanvas(myWindowWidth, myWindowHeight, WEBGL);
-
-    document.getElementById('loadBtn').onclick = () => {
-        const t0 = performance.now();
-        httpGet(BACK_END_URL + '/big', data => {
-            loadImage(BACK_END_URL + data, img => {
-                this.img = img;
-                this.img_loaded = true;
-            })
-        })
-        const t1 = performance.now();
-        print(`Loading images images took: ${(t1 - t0)} milliseconds.`);
-    };
-
-    document.getElementById('rndBtn').onclick = () => {
-        const t0 = performance.now();
-        loadImage(BACK_END_URL + '/random', img => {
-            this.img = img;
-        })
-        const t1 = performance.now();
-        print(`Random order images took: ${(t1 - t0)} milliseconds.`);
-    };
-
-    document.getElementById('revBtn').onclick = () => {
-        const t0 = performance.now();
-        loadImage(BACK_END_URL + '/reverse', img => {
-            this.img = img;
-        })
-        const t1 = performance.now();
-        print(`Reverse order images took: ${(t1 - t0)} milliseconds.`);
-    };
-
-    // diplay the images on a histogram, grouped by dates
-    document.getElementById('histBtn').onclick = () => {
-        const t0 = performance.now();
-        loadImage(BACK_END_URL + '/histogram', img => {
-            this.img = img;
-        })
-        const t1 = performance.now();
-        print(`Histogram images took: ${(t1 - t0)} milliseconds.`);
-    };
 }
 
 function mouseWheel(event) {
@@ -96,10 +112,10 @@ function keyDown() {
 
 function mouseClicked() {
     if (this.img_loaded) {
-        if ((mouseX - myWindowWidth/2) < zoom/2 + posX && mouseX - myWindowWidth/2 > -zoom/2 + posX
-            && (mouseY - myWindowHeight/2) < zoom/2 + posY && (mouseY - myWindowHeight/2) > -zoom/2 + posY) {
-                print('clicked on image')
-                print(mouseX - myWindowWidth/2, mouseY - myWindowHeight/2)
+        if ((mouseX - myWindowWidth / 2) < zoom / 2 + posX && mouseX - myWindowWidth / 2 > -zoom / 2 + posX
+            && (mouseY - myWindowHeight / 2) < zoom / 2 + posY && (mouseY - myWindowHeight / 2) > -zoom / 2 + posY) {
+            print('clicked on image')
+            print(mouseX - myWindowWidth / 2, mouseY - myWindowHeight / 2)
         }
     }
 }
