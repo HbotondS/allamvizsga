52ea4df64a89a9b5b0bd12ce4daa9efc34d8d8d4
HbotondS <hbotond97@gmail.com>
2020-05-29 16:25:38 +0300

    add some comment for json testing

--- testing/testing_json.py
+++ testing/testing_json.py
@@ -1,7 +1,10 @@
 import json
 
+# open json
 with open('images/ImageDataset.TwitterFDL2015.json', encoding="utf8") as f:
+    # stripping the newline character
     lines = [line.rstrip() for line in f]
 
+# convert string to json
 data = [json.loads(line) for line in lines]
 print(data[0]["id_str"])
