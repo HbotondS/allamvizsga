47f97ce1a8ca16a56f15bf3e088016590875dd75
HbotondS <hbotond97@gmail.com>
2020-06-06 20:56:21 +0300

    process json
    also added some comment

--- testing/testing_json.py
+++ testing/testing_json.py
@@ -7,6 +7,12 @@ with open('images/ImageDataset.TwitterFDL2015.json', encoding="utf8") as f:
     lines = [line.rstrip() for line in f]
 
 # convert string to json
-data = [json.loads(line) for line in lines]
-print(data[0]["id_str"])
-print(time.gmtime(int(data[0]["timestamp_ms"]) / 1000.).tm_year)
+# id will be the root node
+data = {}
+for line in lines:
+    line_data = json.loads(line)
+    data[str(line_data['id_str'])] = line_data
+
+print(data['673849510750257152']['id_str'])
+# convert epoch timestamp into gmtime
+print(time.gmtime(int(data['673849510750257152']['timestamp_ms']) / 1000.).tm_year)
